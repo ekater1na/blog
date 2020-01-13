b3f021f1a8000abe7570e8d462210caf2c1b8686
ekater1na <ekaterina_l@tut.by>
2020-01-13 19:23:08 +0300

    feat:  add login component

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { LoginService } from './login.service';
+import { User } from '../models/user.model';
+ 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [ LoginService ]
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class LoginComponent {
+ 
+  public user : User;
+ 
+  constructor(private loginService: LoginService) {
+      this.user = new User();
   }
-
+ 
+  validateLogin() {
+    if(this.user.username && this.user.password) {
+        this.loginService.validateLogin(this.user).subscribe(result => {
+        console.log('result is ', result);
+      }, error => {
+        console.log('error is ', error);
+      });
+    } else {
+        alert('enter user name and password');
+    }
+  }
+ 
 }
