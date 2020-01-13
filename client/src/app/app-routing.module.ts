61e4f15f0f5b49a193582d4c7a1434c7d8e8046e
ekater1na <ekaterina_l@tut.by>
2020-01-13 19:23:26 +0300

    feat: add home; component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
 export const AppRoutes: Routes = [
-	// { path: '', component: LoginComponent },
+	{ path: '', component: LoginComponent },
+	{ path: 'home', component: HomeComponent }
 ];
 
 export const ROUTING: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,21 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ROUTING } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { RootComponent } from './root/root.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
   	RootComponent,
-  	LoginComponent,
-    
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     ROUTING,
     FormsModule,
-    
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [RootComponent]
