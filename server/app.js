97dc3fa211f2f599d30cdd197ff597e69c46a994
ekater1na <ekaterina_l@tut.by>
2020-01-14 12:13:11 +0300

    feat: add mongoose

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+const app = express()
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost/blogDb';
  
-app.get('/api/user/login', (req, res) => {
-    res.send('Hello World!')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended : false}))
+ 
+app.post('/api/user/login', (req, res) => {
+ mongoose.connect(url, function(err){
+ if(err) throw err;
+ console.log('connected successfully, username is ',req.body.username,' password is ',req.body.password);
+ });
 })
  
 app.listen(3000, () => console.log('blog server running on port 3000!'))
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended : false}))
